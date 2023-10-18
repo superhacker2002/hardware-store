e7224d2cf038a9f8a21df9893eca0321dde97903
rubiezzy <rubiezzy@student.21-school.ru>
2023-10-18 20:49:36 +0300

    Add connection to database

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/superhacker2002/shop/internal/config"
+	"github.com/superhacker2002/shop/internal/repository"
+	"log"
+)
+
 func main() {
-	
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatalf("failed to load config %v", err)
+	}
+
+	db, err := sql.Open("postgres", cfg.Db)
+	if err != nil {
+		log.Fatalf("failed to open connection with database: %v", err)
+	}
+	defer db.Close()
+
+	repo := repository.New(db)
+	_, err = repo.ShelfProducts()
+	if err != nil {
+		log.Fatalf("failed to get info from database: %v", err)
+	}
 }
